ebd9e99c3fa9b711f3edce1d683dd8b84438d5b1
ii masamitsu <ii@entap.co.jp>
2020-12-04 08:53:31 +0900

    Seederがうまく読めなかったので調整した

---
+++ database/seeders/AdminTablesSeeder.php
@@ -0,0 +1,44 @@
+<?php
+namespace Database\Seeders;
+
+use Entap\Admin\Database\Models\Operation;
+use Entap\Admin\Database\Models\Permission;
+use Entap\Admin\Database\Models\Role;
+use Entap\Admin\Database\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminTablesSeeder extends Seeder
+{
+    public function run()
+    {
+        $role = $this->createSuperRole();
+
+        $user = User::create([
+            'name' => 'admin',
+            'username' => 'admin',
+            'password' => Hash::make('password'),
+        ]);
+        $user->roles()->save($role);
+    }
+
+    private function createSuperRole()
+    {
+        $operation = Operation::create([
+            'method' => 'any',
+            'action' => '*',
+        ]);
+
+        $permission = Permission::create([
+            'name' => 'full access',
+        ]);
+        $permission->operations()->save($operation);
+
+        $role = Role::create([
+            'name' => 'administrator',
+        ]);
+        $role->permissions()->save($permission);
+
+        return $role;
+    }
+}
