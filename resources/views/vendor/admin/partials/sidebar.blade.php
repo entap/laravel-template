c23ffa1bd97e94f671b465a7faae0a95d9a3b6f9
ii masamitsu <ii@entap.co.jp>
2020-12-07 16:02:03 +0900

    並び順とリンクの有無を調整した

--- resources/views/vendor/admin/partials/sidebar.blade.php
+++ resources/views/vendor/admin/partials/sidebar.blade.php
@@ -1,6 +1,6 @@
 <nav class="nav flex-column">
-    @foreach (App\Models\MenuItem::all() as $item)
-        <a href="{{ $item->uri }}" class="nav-link">
+    @foreach (App\Models\MenuItem::all()->sortBy('order', SORT_NATURAL) as $item)
+        <a href="{{ $item->uri ?? '#' }}" class="nav-item nav-link {{ $item->uri ? '' : 'disabled' }}">
             {{ $item->title }}
         </a>
     @endforeach
