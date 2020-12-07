07cd0b79d46cef1f79886f7aa5bbea3009d181f1
ii masamitsu <ii@entap.co.jp>
2020-12-07 10:03:10 +0900

    パッケージ周りを移したので削除する

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Models\Package;
-use App\Observers\PackageObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +23,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Package::observe(PackageObserver::class);
     }
 }
