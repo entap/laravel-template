ef6697f7973ef980291778ba98f240611164dfa7
ii masamitsu <ii@entap.co.jp>
2020-12-07 15:12:30 +0900

    メニュー項目一覧を追加した

--- resources/views/admin/menu/items/index.blade.php
+++ resources/views/admin/menu/items/index.blade.php
@@ -1,8 +1,20 @@
 @extends('admin::layouts.sidebar')
 
 @section('content')
-    @foreach ($items as $item)
-        {{ $item->title }}
-        {{ $item->uri }}
-    @endforeach
+    <table class="table">
+        <thead>
+            <tr>
+                <th>{{ __('Title') }}</th>
+                <th>{{ __('URL') }}</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($items as $item)
+                <tr>
+                    <td>{{ $item->title }}</td>
+                    <td>{{ $item->uri }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
--- resources/views/vendor/admin/partials/sidebar.blade.php
+++ resources/views/vendor/admin/partials/sidebar.blade.php
@@ -2,4 +2,5 @@
     <a href="{{ route('admin.users.index') }}" class="nav-link">管理者</a>
     <a href="{{ route('admin.roles.index') }}" class="nav-link">権限セット</a>
     <a href="{{ route('admin.packages.index') }}" class="nav-link">パッケージ</a>
+    <a href="{{ route('admin.menu.items.index') }}" class="nav-link">メニュー項目</a>
 </nav>
