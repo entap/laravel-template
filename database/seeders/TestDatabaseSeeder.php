7d33cef603614b87216c39b440675e9658410a41
ii masamitsu <ii@entap.co.jp>
2020-12-09 08:20:28 +0900

    管理ページのテストデータを作った

--- database/seeders/TestDatabaseSeeder.php
+++ database/seeders/TestDatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Entap\ClientPackager\Models\Package;
 use Entap\Admin\Database\Models\MenuItem;
+use Entap\Admin\Database\Models\Permission;
+use Entap\Admin\Database\Models\Role;
+use Entap\Admin\Database\Models\User as AdminUser;
 use Entap\ClientPackager\Models\PackageRelease;
 
 class TestDatabaseSeeder extends Seeder
@@ -31,5 +34,44 @@ class TestDatabaseSeeder extends Seeder
         Package::factory(3)
             ->hasReleases(3)
             ->create();
+
+        $usersPermission = Permission::create(['name' => 'admin.users']);
+        $usersPermission
+            ->operations()
+            ->create(['method' => '*', 'action' => '/admin/auth/users*']);
+        $rolesPermission = Permission::create(['name' => 'admin.roles']);
+        $rolesPermission
+            ->operations()
+            ->create(['method' => '*', 'action' => '/admin/auth/roles*']);
+        $packagesPermission = Permission::create(['name' => 'admin.packages']);
+        $packagesPermission
+            ->operations()
+            ->create(['method' => '*', 'action' => '/admin/packages']);
+
+        $authRole = Role::create(['name' => 'Auth administrator']);
+        $authRole->permissions()->save($usersPermission);
+        $authRole->permissions()->save($rolesPermission);
+
+        $packageRole = Role::create(['name' => 'Package administrator']);
+        $packageRole->permissions()->save($packagesPermission);
+
+        $authAdministrator = AdminUser::factory()->create();
+        $authAdministrator->roles()->save($authRole);
+        $this->command->info(
+            'Auth administrator username: ' . $authAdministrator->username
+        );
+
+        $packageAdministrator = AdminUser::factory()->create();
+        $packageAdministrator->permissions()->save($packagesPermission);
+        $this->command->info(
+            'Package administrator username: ' . $authAdministrator->username
+        );
+
+        $superAdministrator = AdminUser::factory()->create();
+        $superAdministrator->roles()->save($authRole);
+        $superAdministrator->roles()->save($packageRole);
+        $this->command->info(
+            'Super administrator username: ' . $authAdministrator->username
+        );
     }
 }
