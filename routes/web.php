d1d9897a9e1ac3ea5975a87522fb41c44da4de59
ii masamitsu <ii@entap.co.jp>
2020-12-08 10:25:12 +0900

    メニューをlaravel-adminに移した

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::group(
-    [
-        'prefix' => 'admin/menu',
-        'middleware' => ['web'],
-    ],
-    function () {
-        Route::resource('items', MenuItemController::class)->names(
-            'admin.menu.items'
-        );
-    }
-);
