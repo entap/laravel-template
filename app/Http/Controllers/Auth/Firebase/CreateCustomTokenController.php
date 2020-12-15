79699db4bc5292d60d41fdb2a750fdc09dfda4e9
ii masamitsu <ii@entap.co.jp>
2020-12-15 10:17:48 +0900

    カスタムトークンを作るコントローラーを用意した

---
+++ app/Http/Controllers/Auth/Firebase/CreateCustomTokenController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Auth\Firebase;
+
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use UserCreateFirebaseCustomToken;
+
+class CreateCustomTokenController extends Controller
+{
+    protected $customTokenService;
+
+    public function __construct(
+        UserCreateFirebaseCustomToken $customTokenService
+    ) {
+        $this->customTokenService = $customTokenService;
+    }
+
+    public function __invoke(Request $request)
+    {
+        $user = $request->user();
+        $customToken = $this->customTokenService->create(
+            $user->firebase_id ? $user->firebase_id : Str::uuid()
+        );
+        return [
+            'custom_token' => $customToken,
+        ];
+    }
+}
---
+++ app/UseCases/UserCreateFirebaseCustomToken.php
@@ -0,0 +1,18 @@
+<?php
+
+use Kreait\Firebase\Auth;
+
+class UserCreateFirebaseCustomToken
+{
+    protected $auth;
+
+    public function __construct(Auth $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    public function create(string $uid): string
+    {
+        return (string) $this->auth->createCustomToken($uid);
+    }
+}
