b64206fd370d738351c218afa374f327cd762244
ii masamitsu <ii@entap.co.jp>
2020-12-18 09:38:33 +0900

    LINEのコントローラーを取り込んだ

---
+++ app/Http/Controllers/Auth/Line/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Auth\Line;
+
+use App\Gateways\Line\VerifyIdTokenGateway;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    protected $line;
+
+    public function __construct(VerifyIdTokenGateway $line)
+    {
+        $this->line = $line;
+    }
+
+    public function __invoke(Request $request)
+    {
+        $request->validate([
+            'id_token' => 'required',
+        ]);
+
+        $uid = $this->line->verify($request->input('id_token'));
+
+        $user = User::withProvider('line', $uid)->firstOrCreate();
+        $user->saveProvider('line', $uid);
+
+        $token = $user->createToken('LINE Token');
+
+        return [
+            'access_token' => $token->accessToken,
+        ];
+    }
+}
