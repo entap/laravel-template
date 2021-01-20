d6595ded4f5e98bbc024ff76ef305ebf1334717e
ii masamitsu <ii@entap.co.jp>
2021-01-20 11:41:22 +0900

    基本パッケージをまとめた

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,10 +41,6 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
-# Bootstrap Pagination
-
-Illuminate\Pagination\AbstractPaginator::defaultView("pagination::bootstrap-4");
-
 /*
 |--------------------------------------------------------------------------
 | Return The Application
