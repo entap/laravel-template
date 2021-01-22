e4ed838d41d3673b29bb6057bc2656654b1731ea
ii masamitsu <ii@entap.co.jp>
2021-01-22 09:03:31 +0900

    メール送信コマンドの例を用意した

---
+++ app/Console/Commands/Examples/SendMail.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands\Examples;
+
+use Entap\Admin\Application\Services\SendMailTemplateService;
+use Entap\Admin\Database\Models\MailTemplate;
+use Entap\Admin\Database\Models\MailType;
+use Illuminate\Console\Command;
+
+class SendMail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'example:mail:send';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send e-mail example';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $type = MailType::factory()->create();
+        $template = MailTemplate::factory()->create([
+            'mail_type_id' => $type->id,
+            'subject' => 'Hi, {company.name}.',
+            'body' => '{company.name} is great!',
+        ]);
+        (new SendMailTemplateService())->sendByTypeCode($type->code, [
+            'company' => [
+                'name' => 'Entap',
+            ],
+        ]);
+
+        return 0;
+    }
+}
