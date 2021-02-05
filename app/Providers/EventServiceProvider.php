644c18602223d34bf2a10bfb88342778e563db4c
ii masamitsu <ii@entap.co.jp>
2021-02-05 09:38:17 +0900

    不要な読み込みを外した

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,11 +2,9 @@
 
 namespace App\Providers;
 
-use App\Listeners\MessageLogger;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Mail\Events\MessageSent;
 use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
