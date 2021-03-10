2a2255830a23840fbc5223b2572d4cd76660f780
ii masamitsu <ii@entap.co.jp>
2021-03-11 06:03:09 +0900

    deployerを設定する

--- deploy.php
+++ deploy.php
@@ -4,31 +4,41 @@ namespace Deployer;
 require 'recipe/laravel.php';
 
 // Project name
-set('application', 'my_project');
+set('application', 'grasshopper');
 
 // Project repository
-set('repository', 'https://github.com/entap/laravel-template.git');
+set('repository', 'git@github.com:entap/laravel-template.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true); 
+set('git_tty', true);
 
-// Shared files/dirs between deploys 
-add('shared_files', []);
-add('shared_dirs', []);
+// Shared files/dirs between deploys
+add('shared_files', ['.env']);
+add('shared_dirs', ['storage']);
 
-// Writable dirs by web server 
-add('writable_dirs', []);
-
+// Writable dirs by web server
+add('writable_dirs', ['bootstrap/cache', 'storage']);
 
 // Hosts
 
-host('project.com')
-    ->set('deploy_path', '~/{{application}}');    
-    
+host('grasshopper.entap.dev')
+    ->stage('development')
+    ->user('grasshopper')
+    ->set('deploy_path', '~/{{application}}');
+
 // Tasks
 
 task('build', function () {
     run('cd {{release_path}} && build');
+});
+
+desc('Upload local .env file.');
+task('deploy:dotenv', function () {
+    $src = __DIR__ . '/.env.{{target}}';
+    $dst = '{{deploy_path}}/shared/.env';
+    $hostname = '{{hostname}}';
+    upload($src, $dst);
+    writeln("uploaded {$src} to {$hostname}:{$dst}");
 });
 
 // [Optional] if deploy fails automatically unlock.
@@ -37,4 +47,3 @@ after('deploy:failed', 'deploy:unlock');
 // Migrate database before symlink new release.
 
 before('deploy:symlink', 'artisan:migrate');
-
