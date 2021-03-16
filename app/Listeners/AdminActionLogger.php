b2b0c84915950af3caa9d6f92af51a02929c4247
ii masamitsu <ii@entap.co.jp>
2021-03-17 06:20:08 +0900

    ロガーの雛形を追加

---
+++ app/Listeners/AdminActionLogger.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Models\LogAdminActionEntry;
+use Entap\Admin\Facades\Admin;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class AdminActionLogger
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    // public function handleUserCreated(UserCreated $event)
+    // {
+    //     LogAdminActionEntry::create([
+    //         'admin_user_id' => optional(Admin::user())->id,
+    //         'action' => 'users.created',
+    //     ]);
+    // }
+
+    public function subscribe($events)
+    {
+        // $events->listen(UserCreated::class, [self::class, 'handleUserCreated']);
+    }
+}
