9c17432cbff1fa29bc74af15ac9711fc9fd88a96
ii masamitsu <ii@entap.co.jp>
2021-03-24 15:10:28 +0900

    余計なuseを削除

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,9 @@
 <?php
 
-use Entap\Admin\Facades\Admin;
 use Illuminate\Support\Facades\Route;
-use App\Admin\Controllers\UserController;
-use App\Admin\Controllers\EntryController;
-use App\Admin\Controllers\TableController;
-use App\Admin\Controllers\PackageController;
-use App\Http\Controllers\AdminJobController;
-use App\Admin\Controllers\DynamicPageController;
-use App\Admin\Controllers\UserOpinionController;
-use App\Admin\Controllers\UserSegmentController;
-use App\Admin\Controllers\TemporaryUserController;
-use App\Admin\Controllers\DynamicContentController;
-use App\Admin\Controllers\PackageReleaseController;
 use App\Http\Controllers\ShowDynamicPageController;
-use App\Admin\Controllers\DynamicCategoryController;
 use App\Http\Controllers\FixTemporaryUserController;
-use App\Http\Controllers\Admin\SuspendUserController;
-use App\Http\Controllers\Admin\AgreementTypeController;
-use App\Http\Controllers\Admin\UnsuspendUserController;
-use App\Admin\Controllers\AcceptTemporaryUserController;
-use App\Admin\Controllers\RejectTemporaryUserController;
 use App\Http\Controllers\RegisterTemporaryUserController;
-use App\Http\Controllers\Admin\AgreementTypeAgreementController;
 
 /*
 |--------------------------------------------------------------------------
