d4b856cc8dafe6f6c7b1bf431ef4c289c0517646
ii masamitsu <ii@entap.co.jp>
2021-04-09 08:14:41 +0900

    add comments

--- app/Models/GroupUser.php
+++ app/Models/GroupUser.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 use Spatie\Permission\Traits\HasRoles;
 
+/**
+ * グループのメンバー
+ */
 class GroupUser extends Pivot
 {
     use HasRoles;
