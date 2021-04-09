69e259245e758c34d3e360b218d9a59acbff3c60
ii masamitsu <ii@entap.co.jp>
2021-04-09 10:01:29 +0900

    rename

--- tests/Feature/Ladybird/UserRemoveUserFromGroupTest.php
+++ tests/Feature/Ladybird/UserRemoveUserFromGroupTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class UserRemoveUserTest extends TestCase
+class UserRemoveUserFromGroupTest extends TestCase
 {
     /**
      * A basic feature test example.
